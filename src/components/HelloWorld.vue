576584aac3b06f86199fbc7c6730d1ae1e457908
corentinmace <corentin.mace@free.fr>
2022-08-09 04:23:58 +0200

    Base project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<script>
+import { ref } from 'vue'
+export default {
+	setup() {
+
+		const text = ref('')
+		const formatedText = ref('')
+		let phrases = []
+
+		const formatText = () => {
+			formatedText.value = text.value.split().reduce((a, e, i)=> a + e + (i % 39 === 0 ? '\\n' : ''), '');
+		}
+
+		
+
+
+		return {
+			text,
+			formatedText,
+			formatText
+		}
+
+	}
+}
+</script>
+
+<template>
+	<input type="text" @input="formatText()" v-model="text">
+	<p>	
+		{{ formatedText}}
+	</p>
+</template>
