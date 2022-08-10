c48bbec47bc8a61b5efcdb9748c78a0fc8a66f59
Marianne Corbel <mariannecorbel@hotmail.fr>
2022-08-10 04:29:59 +0200

    HTML & DSPRE formatting functions

--- src/composables/formatComposable.js
+++ src/composables/formatComposable.js
@@ -4,56 +4,67 @@
 
 const formatComposable = () => {
 
-    const reverse = (str) => {
+    const remove_whitespaces = (str) => {
         return str.replace(/\f|\r\n|\n|\r/gm, ' ');
     }
     
-    const format = (str, index=39, lines=2) => {
-        let args = [str, index, lines]
-        // Non-valid arguments:
-        if (args.length === 0 || typeof args[0] !== "string" || args[0].length === 0) {
-            console.log("Sorry, it didn't work...\n>>>[0]: non-empty string;\n>>>[1]: splitting index (opt.);\n>>>[2]: Nb of sentences on screen (opt.).");
-            return 1;
-        }
-    
-        str = reverse(str);
-        // SEPARATING THE STRING IN CHUNKS:
-        let chunks = [];
-        while (str.length > index) {
+    const text_to_DSPRE = (str="", flow=true, index=39, lines=2) => {
+
+        // Removes potential whitespaces from str.
+        str = remove_whitespaces(str)
+
+        // PART 1: DIVIDES STR INTO CHUNKS OF {index} CHARACTERS [default: 39]:
+        let chunks = []; 
+        // Creates a copy of str without the potential color elements:
+        const generate_copy = () => { return str.slice().replace(/\\vFF00\\x0001(\\x0001|あ|ぁ)|\\vFF00\\x0001\\x0000/g, '')}
+        let copy = generate_copy();
+
+        while (copy.length > index) {
             // Case 1: Chunk constituted of at least two words, splits on the last whitespace of the chunk.
-            if (str.slice(0, index +1 ).lastIndexOf(' ') !== -1) {
-                chunks.push(str.slice(0, str.slice(0, index +1 ).lastIndexOf(' '))); 
-                str = str.slice(str.slice(0, index +1 ).lastIndexOf(' ')).replace(/^\s+|$\s+/g, '');
-    
+            if (/ /.test(copy.slice(0, index + 1))) {
+                let last_space_index = [...copy.slice(0, index + 1).matchAll(' ')].length - 1;
+                chunks.push(str.slice(0, [...str.matchAll(' ')][last_space_index].index));
+                str = str.slice([...str.matchAll(' ')][last_space_index].index).replace(/^\s+|$\s+/g, '');
+
             // Case 2: Chunk is a very long word, splits on the 'index-th' letter of the word.
             } else {
-                chunks.push(str.slice(0, index)); str = str.slice(index).replace(/^\s+|$\s+/g, '');
+                let count = str.match(/^\\vFF00\\x0001(\\x0001|あ|ぁ)/g) === null 
+                ? 0 : str.match(/^\\vFF00\\x0001(\\x0001|あ|ぁ)/g)[0].length;
+                chunks.push(str.slice(0, index + count)); str = str.slice(index + count).replace(/^\s+|$\s+/g, '');
             }
+            // Updates the copy to the now updated str.
+            copy = generate_copy();
+
         }
-        chunks.push(str.replace(/^\s+|$\s+/g, ''));
-    
-        // ADDING NEWLINES CHAR WHEN MORE THAN 1 CHUNK:
+        // Adds the last chunk (length < index) to the chunk list.
+        chunks.push(str.replace(/^\s+|\s+$/g, ''));
+
+        // PART 2: ADDS NEWLINE CHARACTERS AT THE END OF EACH CHUNK:
         if (chunks.length >= 1) {
             let last_chunk = chunks.pop();
-            chunks = chunks.map((chunk, index) => (index + 1) % lines !== 0 ? chunk += `\\n` : chunk += `\\r`)
+            chunks = chunks.map((chunk, index) => {
+                return flow 
+                    ? ((index + 1) % lines !== 0 ? chunk += `\\n` : chunk += `\\f`)
+                    : ((index + 1) % lines !== 0 ? chunk += `\\n` : chunk += `\\r`)
+            });
             chunks.push(last_chunk);
-        }
-    
+        };
+
         // RESULT:
-        console.log(chunks)
         return chunks.join('')
     }
 
-    const generate_preview = (str) => {
+    const DSPRE_to_HTML = (str) => {
+        // Replaces color elements as written in DSPRE with HTML-readable CSS 
+        //  and removes the extra escape characters.
+
         var colors = { 
-            "\\x0001": "red",
-            "ぁ": "blue",
-            "あ": "green",
-        }
+            "\\x0001": "red", "ぁ": "blue", "あ": "green",
+        };
         var whitespaces = {
             "\\n":"\n", "\\r":"\r", "\\f": "\f"
-        }
-        let regex = /\\vFF00\\x0001(\\x0001|あ|ぁ)(.*)\\vFF00\\x0001\\x0000!/
+        };
+        let regex = /\\vFF00\\x0001(\\x0001|あ|ぁ)(.*)\\vFF00\\x0001\\x0000/g
     
         if (str.match(regex) !== null) {
             str = str
@@ -62,26 +73,40 @@ const formatComposable = () => {
                 .replace(/\\x0001|あ|ぁ/g, function(matched) {
                     return `${colors[matched]}">`;
                 });
-        }
+        };
         return str.replace(/\\n|\\r|\\f/g, function(matched) {
             return whitespaces[matched];
-        })
+        });
+    ;}
+
+    const format = (str, flow=false, index=39, lines=2) => {
+        // SECURITY CHECKS:
+        if (typeof str !== 'string' || typeof flow !== 'boolean'
+        || typeof index !== 'number' || typeof lines !== 'number') {
+            console.log("Something went wrong...\n>>>[0]: non-empty string;\n>>>[1]: flow type bool [\\r or \\f] (opt., default=true -> \\r)" +
+            "\n>>>[2]: splitting index (opt., default=39)\n>>>[2]: number of lines (opt., default=2)")
+            return 1;
+        }
+
+        let DSPRE_formatted = text_to_DSPRE(str, flow, index, lines);
+        let HTML_formatted = DSPRE_to_HTML(DSPRE_formatted);
+
+        return {DSPRE_formatted, HTML_formatted}
     }
-    
-        // EXAMPLES
+
+    // EXAMPLES:
     const sentence_1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    const sentence_2 = "I just had this idea. Maybe you can pull it off. It’s about this town’s Gym Leader. He’s really lost his enthusiasm lately because there’ve been so few good challengers worth his time. He’s been so bored, he spends all his time renovating the Gym. And then the blackout happened... That sure didn’t help lift his mood. You know where I’m going with this, don’t you? I need you to give him a battle that’s so hot, it will reignite his passion for battling. I’m counting on you, hotshot!"
+    const sentence_2 = "I just had this idea.\nMaybe you can pull it off.\rIt’s about this town’s Gym Leader.\rHe’s really lost his enthusiasm lately\nbecause there’ve been so few good\fchallengers worth his time.\rHe’s been so bored, he spends all his\ntime renovating the Gym.\rAnd then the blackout happened...\nThat sure didn’t help lift his mood.\rYou know where I’m going with this,\ndon’t you?\rI need you to give him a battle\nthat’s so hot, it will reignite his\fpassion for battling.\rI’m counting on you, hotshot!\r"
     const sentence_3 = "HELLOOOOOOOOOOOOOOOOOOOOOOOO (yeah that's a shitty exemple)"
-    const reverse_1 = "I just had this idea.\nMaybe you can pull it off.\rIt’s about this town’s Gym Leader.\rHe’s really lost his enthusiasm lately\nbecause there’ve been so few good\fchallengers worth his time.\rHe’s been so bored, he spends all his\ntime renovating the Gym.\rAnd then the blackout happened...\nThat sure didn’t help lift his mood.\rYou know where I’m going with this,\ndon’t you?\rI need you to give him a battle\nthat’s so hot, it will reignite his\fpassion for battling.\rI’m counting on you, hotshot!\r"
-    
-    console.log(format())
+    const color_1 = "\\vFF00\\x0001ぁPenguin Pokémon PIPLUP!\\vFF00\\x0001\\x0000! Will you take this Pokémon?"
+    const color_2 = "\\vFF00\\x0001\\x0001Chimp Pokémon CHIMCHAR!\\vFF00\\x0001\\x0000!\\n Will you take this Pokémon?"
+    const color_3 = "\\vFF00\\x0001あTiny Leaf Pokémon TURTWIG!\\vFF00\\x0001\\x0000!\\r Will you take this Pokémon?"
 
     return {
+        remove_whitespaces,
+        text_to_DSPRE, 
+        DSPRE_to_HTML,
         format,
-        reverse, 
-        generate_preview
     }
-
 }
-
 export default formatComposable
