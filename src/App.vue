f8819d371ab18a3f77cc36f8a48b4c8bb9b6654d
corentinmace <corentin.mace@free.fr>
2022-08-10 15:15:07 +0200

    Interface update : Select lines, charaters and preset

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import FormatComposable from './composables/formatComposable' 
 export default {
 	setup() {
@@ -11,6 +11,17 @@ export default {
 		const showColorText = ref(false)
 		const selectedColor = ref('')
 		const checkToFlow = ref(false)
+		const selectPreset = ref('39,2')
+		const charsSettings = ref('')
+		const linesSettings = ref('') 
+
+		
+
+		const defineLinesAndChar = () => {
+			let values = selectPreset.value.split(',')
+			charsSettings.value = values[0]
+			linesSettings.value = values[1]
+		}
 
 		const { format } = FormatComposable()
 
@@ -19,9 +30,13 @@ export default {
 			selectedColor.value = ''
 			showColorText.value = false
 		}
+
+		const handleSelect = () => {
+			defineLinesAndChar()
+		}
 	
 		const handleChange = () => {
-			let formatValue = format(text.value, checkToFlow.value)
+			let formatValue = format(text.value, checkToFlow.value, JSON.parse(charsSettings.value), JSON.parse(linesSettings.value))
 			previewText.value = formatValue.HTML_formatted
 			formatedText.value = formatValue.DSPRE_formatted
 		}
@@ -58,6 +73,11 @@ export default {
 			showColorText.value = true
 		}
 
+		onMounted(() => {
+			defineLinesAndChar()
+		})
+
+
 		return {
 			text,
 			formatedText,
@@ -67,7 +87,11 @@ export default {
 			showColorText,
 			colorCodeText,
 			addColorCodes,
-			checkToFlow
+			checkToFlow,
+			selectPreset,
+			handleSelect,
+			charsSettings,
+			linesSettings
 		}
 
 	}
@@ -81,11 +105,23 @@ export default {
 			<textarea id="input" class="min-h-[200px] placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" @input="handleChange()" v-model="text"></textarea>
 			<div class="flex items-center justify-between h-8 mt-3 mx-2">
 				<div class="flex">
-					<div @click="setColor('red')" class="h-8 w-8 cursor-pointer rounded-lg bg-red-600 border border-slate-300 mr-3"></div>
-					<div @click="setColor('green')" class="h-8 w-8 cursor-pointer rounded-lg bg-green-600 border border-slate-300 mr-3"></div>
-					<div @click="setColor('blue')" class="h-8 w-8 cursor-pointer rounded-lg bg-blue-600 border border-slate-300 mr-3"></div>
+					<div @click="setColor('red')" class="h-6 w-6 cursor-pointer rounded-lg bg-red-600 border border-slate-300 mr-3"></div>
+					<div @click="setColor('green')" class="h-6 w-6 cursor-pointer rounded-lg bg-green-600 border border-slate-300 mr-3"></div>
+					<div @click="setColor('blue')" class="h-6 w-6 cursor-pointer rounded-lg bg-blue-600 border border-slate-300 mr-3"></div>
 				</div>
 				<div class="flex items-center">
+					<label for="select-setter" class=" text-sm font-medium text-gray-900 dark:text-gray-300 tracking-wide">Preset</label>
+					<select @change="handleSelect" v-model="selectPreset" id="select-setter" class="ml-2 mr-3 h-6 pl-2 text-white bg-gray-100 rounded border-gray-300 focus:ring-sky-500 dark:focus:ring-sky-600 dark:ring-offset-sky-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+						<option value="39,2" selected>Overworld</option>
+						<option value="38,3">PokeDex</option>
+					</select>
+
+					<label for="line-setter" class=" text-sm font-medium text-gray-900 dark:text-gray-300 tracking-wide">Lines</label>
+					<input v-model="linesSettings" id="line-setter" type="number" class="ml-2 mr-3 w-12 h-4 py-3 pl-2 text-white bg-gray-100 rounded border-gray-300 focus:ring-sky-500 dark:focus:ring-sky-600 dark:ring-offset-sky-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+
+					<label for="char-setter" class=" text-sm font-medium text-gray-900 dark:text-gray-300 tracking-wide">Characters</label>
+					<input v-model="charsSettings" id="char-setter" type="number" class="ml-2 mr-3 w-12 h-4 pl-2 py-3 text-white bg-gray-100 rounded border-gray-300 focus:ring-sky-500 dark:focus:ring-sky-600 dark:ring-offset-sky-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+
 					<label for="link-checkbox" class=" text-sm font-medium text-gray-900 dark:text-gray-300 tracking-wide">Transform <span class="font-bold text-white">\r</span> to  <span class="font-bold text-white">\f</span></label>
 					<input id="link-checkbox" type="checkbox" v-model="checkToFlow" class="ml-2 w-4 h-4 text-sky-600 bg-gray-100 rounded border-gray-300 focus:ring-sky-500 dark:focus:ring-sky-600 dark:ring-offset-sky-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
 				</div>
