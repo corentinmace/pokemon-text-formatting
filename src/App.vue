f33856fc50b9b1b42ee1399e0bbb4389613d3d54
corentinmace <corentin.mace@free.fr>
2022-08-13 02:27:32 +0200

    update gen5 interface

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,127 @@
 <script>
 import { ref, onMounted } from 'vue'
 import FormatComposable from './composables/formatComposable' 
+import ColorsComposable from './composables/colorsComposable'
+
 export default {
 	setup() {
 
-		const text = ref('')
-		const formatedText = ref('')
-		const previewText = ref('')
-		const colorCodeText = ref('')
-		const showColorText = ref(false)
-		const selectedColor = ref('')
-		const checkToFlow = ref(false)
-		const selectPreset = ref('39,2')
-		const charsSettings = ref('')
-		const linesSettings = ref('') 
+		const text = ref('');
+		const formatedText = ref('');
+		const previewText = ref('');
+		const colorCodeText = ref('');
+		const showColorText = ref(false);
+		const selectedColor = ref('');
+		const checkToFlow = ref(false);
+		const selectPreset = ref('39,2');
+		const charsSettings = ref('');
+		const linesSettings = ref('');
+		
+		const selectedGen = ref(4);
+		const fifthGenColors = ref([
+			{
+				'name': 'red',
+				'code': 'G5red'
+			},
+			{
+				'name': 'blue',
+				'code': 'G5blue'
+			},
+			{
+				'name': 'yellow',
+				'code': 'G5yellow'
+			},
+			{
+				'name': 'green',
+				'code': 'G5green'
+			},
+			{
+				'name': 'orange',
+				'code': 'G5orange'
+			},
+			{
+				'name': 'pink',
+				'code': 'G5pink'
+			}
+		]);
+
+		const fourthGenColors = ref([
+			{
+				'name': 'red',
+				'code': 'G4red'
+			},
+			{
+				'name': 'green',
+				'code': 'G4green'
+			},
+			{
+				'name': 'blue',
+				'code': 'G4blue'
+			},
+		]);
 
 		const defineLinesAndChar = () => {
-			let values = selectPreset.value.split(',')
-			charsSettings.value = values[0]
-			linesSettings.value = values[1]
+			let values = selectPreset.value.split(',');
+			charsSettings.value = values[0];
+			linesSettings.value = values[1];
 		}
 
-		const { format, rawOutput } = FormatComposable()
+		const { format, rawOutput } = FormatComposable();
+		const { addColorCodes } = ColorsComposable();
 
 		const resetDefault = () => {
-			colorCodeText.value = ''
-			selectedColor.value = ''
-			showColorText.value = false
+			colorCodeText.value = '';
+			selectedColor.value = '';
+			showColorText.value = false;
+		}
+
+		const handleInsert = () => {
+			text.value += addColorCodes(colorCodeText.value, selectedColor.value, selectedGen.value);
+			handleChange();
+			resetDefault();
 		}
 
 		const handleSelect = () => {
-			defineLinesAndChar()
+			defineLinesAndChar();
 		}
 	
 		const handleChange = () => {
-			let formatValue = format(text.value, checkToFlow.value, JSON.parse(charsSettings.value), JSON.parse(linesSettings.value))
-			previewText.value = formatValue.HTML_formatted
-			formatedText.value = formatValue.DSPRE_formatted
-			rawOutput(text.value)
-		}
-
-		const addColorCodes = () => {
-			let haystack = ''
-			switch (selectedColor.value) {
-				case 'red':
-					haystack = '\\vFF00\\x0001\\x0001INSERTTEXT\\vFF00\\x0001\\x0000'
-					haystack = haystack.replace('INSERTTEXT', colorCodeText.value)
-					break;
-
-				case 'green':
-					haystack = '\\vFF00\\x0001あINSERTTEXT\\vFF00\\x0001\\x0000'
-					haystack = haystack.replace('INSERTTEXT', colorCodeText.value)
-					break;
-				
-				case 'blue':
-					haystack = '\\vFF00\\x0001ぁINSERTTEXT\\vFF00\\x0001\\x0000'
-					haystack = haystack.replace('INSERTTEXT', colorCodeText.value)
-					break;
-			
-				default:
-					break;
-			}
-			
-			text.value += haystack
-			handleChange()
-			resetDefault()
+			let formatValue = format(text.value, checkToFlow.value, JSON.parse(charsSettings.value), JSON.parse(linesSettings.value));
+			previewText.value = formatValue.HTML_formatted;
+			formatedText.value = formatValue.DSPRE_formatted;
+			rawOutput(text.value);
 		}
 
 		const setColor = (color) => {
-			selectedColor.value = color
-			showColorText.value = true
+			selectedColor.value = color;
+			showColorText.value = true;
 		}
 
 		onMounted(() => {
-			defineLinesAndChar()
+			defineLinesAndChar();
 		})
 
 
 		return {
 			text,
 			formatedText,
-			handleChange,
 			previewText,
-			setColor,
 			showColorText,
 			colorCodeText,
-			addColorCodes,
+			selectedColor,
 			checkToFlow,
 			selectPreset,
-			handleSelect,
 			charsSettings,
-			linesSettings
+			linesSettings,
+			selectedGen,
+			fourthGenColors,
+			fifthGenColors,
+			resetDefault,
+			setColor,
+			addColorCodes,
+			handleChange,
+			handleSelect,
+			handleInsert,
 		}
 
 	}
@@ -98,15 +129,18 @@ export default {
 </script>
 
 <template>
-	<div class="flex flex-row justfiy-between w-full p-5">
+	<span class="hidden bg-G4red bg-G4green bg-G4blue bg-G5red bg-G5blue bg-G5green bg-G5yellow bg-G5orange bg-G5pink">Let's not show this, but it's required here, don't ask.</span>
+	<div class="fixed top-0 py-2 w-full bg-zinc-800 text-slate-500 text-sm text-center"><span @click="selectedGen = 4" class="font-bold text-slate-400 cursor-pointer" :class="selectedGen == 4 ? 'font-bold' : 'font-normal'">4th Generation</span> - <span @click="selectedGen = 5" class="text-slate-400 cursor-pointer" :class="selectedGen == 5 ? 'font-bold' : 'font-normal'">5th Generation</span></div>
+	<div class="flex flex-row justfiy-between w-full p-5 mt-12">
 		<div class="w-1/2 px-5">
 			<p class="text-lg text-white uppercase font-bold mb-2">Input</p>
 			<textarea id="input" class="min-h-[200px] placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" @input="handleChange()" v-model="text"></textarea>
 			<div class="flex items-center justify-between h-8 mt-3 mx-2">
-				<div class="flex">
-					<div @click="setColor('red')" class="h-6 w-6 cursor-pointer rounded-lg bg-red-600 border border-slate-300 mr-3"></div>
-					<div @click="setColor('green')" class="h-6 w-6 cursor-pointer rounded-lg bg-green-600 border border-slate-300 mr-3"></div>
-					<div @click="setColor('blue')" class="h-6 w-6 cursor-pointer rounded-lg bg-blue-600 border border-slate-300 mr-3"></div>
+				<div class="flex" v-if="selectedGen == 4">
+						<div v-for="color in fourthGenColors" @click="setColor(color.name)" class="h-6 w-6 cursor-pointer rounded-lg border border-slate-300 mr-3" :class="'bg-'+ color.code"></div>
+				</div>
+				<div class="flex" v-if="selectedGen == 5">
+						<div v-for="color in fifthGenColors" @click="setColor(color.name)" class="h-6 w-6 cursor-pointer rounded-lg border border-slate-300 mr-3" :class="'bg-'+ color.code"></div>
 				</div>
 				<div class="flex items-center">
 					<label for="select-setter" class=" text-sm font-medium text-gray-900 text-gray-300 tracking-wide">Preset</label>
@@ -121,11 +155,12 @@ export default {
 					<label title="This is the number of characters needed to break a line" for="char-setter" class=" text-sm font-medium text-gray-900 text-gray-300 tracking-wide">Characters</label>
 					<input v-model="charsSettings" id="char-setter" type="number" class="ml-2 mr-3 w-12 h-4 pl-2 py-3 text-white bg-gray-100 rounded border-gray-300 focus:ring-sky-500 focus:ring-sky-600 ring-offset-sky-800 focus:ring-2 bg-gray-700 border-gray-600">
 
-					<label for="link-checkbox" class=" text-sm font-medium text-gray-900 text-gray-300 tracking-wide">Transform <span class="font-bold text-white">\r</span> to  <span class="font-bold text-white">\f</span></label>
+					<label for="link-checkbox" v-if="selectedGen != 5" class=" text-sm font-medium text-gray-900 text-gray-300 tracking-wide">Transform <span class="font-bold text-white">\r</span> to  <span class="font-bold text-white">\f</span></label>
+					<label for="link-checkbox" v-else class=" text-sm font-medium text-gray-900 text-gray-300 tracking-wide">Transform <span class="font-bold text-white">\c</span> to  <span class="font-bold text-white">\r</span></label>
 					<input id="link-checkbox" type="checkbox" v-model="checkToFlow" class="ml-2 w-4 h-4 text-sky-600 bg-gray-100 rounded border-gray-300 focus:ring-sky-500 focus:ring-sky-600 ring-offset-sky-800 focus:ring-2 bg-gray-700 border-gray-600">
 				</div>
 			</div>
-				<form class="flex items-center" v-if="showColorText" @submit.prevent="addColorCodes">
+				<form class="flex items-center" v-if="showColorText" @submit.prevent="handleInsert">
 					<input  placeholder="The word you want to color" class="mt-3 placeholder:italic text-white block bg-zinc-800 border border-slate-300 rounded-md p-2  shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm mr-2" v-model="colorCodeText">
 					<button class="mt-3 uppercase font-bold text-white block bg-zinc-800 border border-slate-300 rounded-md p-2  shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm">Insert</button>
 				</form>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'G4red': '#ff0000',
+        'G4green': '#00ff00',
+        'G4blue': '#0000ff',
+        'G5red': '#ff383e',
+        'G5blue': '#2093ff',
+        'G5green': '#29c426',
+        'G5yellow': '#f4f425',
+        'G5orange': '#fec326',
+        'G5pink': '#fa10e3'
+      }
+    },
   },
   plugins: [],
 }
